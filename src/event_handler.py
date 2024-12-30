afb6c7f59b385f118b2ff6794244a70ad221488e
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2024-12-30 12:44:19 +0200

    add key events for cloud movement in event_handler.py

--- src/event_handler.py
+++ src/event_handler.py
@@ -32,5 +32,27 @@ class EventHandler:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return False
+            elif event.type == pygame.KEYDOWN:
+                return self._handle_keyboard(event)
+        return True
 
+
+    def _handle_keyboard(self, event) -> bool:
+        """
+        Handle the keyboard events
+
+        Args:
+            event (pygame.event.Event): The keyboard event
+
+        Returns:
+            bool: True if the game should continue, False if the game should end
+        """
+        if event.key == pygame.K_UP:
+            self.game_state.cloud_player2.move("up")
+        elif event.key == pygame.K_DOWN:
+            self.game_state.cloud_player2.move("down")
+        elif event.key == pygame.K_w:
+            self.game_state.cloud_player1.move("up")
+        elif event.key == pygame.K_s:
+            self.game_state.cloud_player1.move("down")
         return True

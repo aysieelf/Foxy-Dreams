6befe8d57bb42bb236ab7eebfce2042673d1e83f
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2025-01-02 17:29:47 +0200

    add hitbox property and handle_fox_collision() to bonus_star.py

--- src/core/bonus_star.py
+++ src/core/bonus_star.py
@@ -8,24 +8,46 @@ class BonusStar(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         self._active = False
         self.sprite_group = sprite_group
+        self.collision_cooldown = 0
 
         self.image = pygame.image.load("assets/images/star-bonus.png").convert_alpha()
         self.rect = self.image.get_rect()
 
+    @property
+    def hitbox(self):
+        box = self.rect.copy()
+        box.width = self.rect.width - c.BONUS_HITBOX_DIFF
+        box.height = self.rect.height - c.BONUS_HITBOX_DIFF
+        box.center = self.rect.center
+        return box
 
     def spawn(self):
         self._active = True
         self._set_pos()
         self.sprite_group.add(self)
-        print("Spawned")
         pygame.time.set_timer(c.BONUS_DE_SPAWN_EVENT, c.BONUS_LIFETIME)
 
     def despawn(self):
         self._active = False
-        print("Despawned")
         self.sprite_group.remove(self)
+        self.collision_cooldown = 0
         pygame.time.set_timer(c.BONUS_DE_SPAWN_EVENT, 0)
 
     def _set_pos(self):
-        self.rect = get_random_position()
+        self.rect.center = get_random_position()
         return self.rect
+
+    def handle_fox_collision(self, fox):
+        # TODO: Add particles
+        if not self._active:
+            return 0
+
+        if self.collision_cooldown > 0:
+            return 0
+
+        if fox.hitbox.colliderect(self.hitbox):
+            self.collision_cooldown = 5
+            self.despawn()
+            return c.BONUS_POINTS
+
+        return 0

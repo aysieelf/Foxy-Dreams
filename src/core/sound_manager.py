4dc2787fa590b0f486a96804705c613488c1e872
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2025-01-03 12:26:46 +0200

    add cooldown for sounds

--- src/core/sound_manager.py
+++ src/core/sound_manager.py
@@ -1,11 +1,17 @@
-from typing import Dict
 import pygame.mixer
 
 class SoundManager:
     def __init__(self):
         self.sounds = {}
-        self.sound_volume = 0.5
-        self.music_volume = 0.3
+        self.sound_volume = 0.3
+        self.music_volume = 0.1
+        self.sound_cooldowns = {}
+        self.cooldown_times = {
+            "fox-bounce": 150,
+            "fox-fly-away": 1000,
+            "score": 0,
+            "bonus": 0
+        }
         self._load_sounds()
         self._setup_music()
 
@@ -25,8 +31,21 @@ class SoundManager:
         pygame.mixer.music.set_volume(self.music_volume)
 
     def play_sound(self, sound_name: str):
-        if sound_name in self.sounds:
-            self.sounds[sound_name].play()
+        current_time = pygame.time.get_ticks()
+
+        if sound_name not in self.sounds:
+            return
+
+        if sound_name in self.sound_cooldowns:
+            print(f"It's in cooldown!")
+            last_played = self.sound_cooldowns[sound_name]
+            cooldown = self.cooldown_times.get(sound_name, 0)
+            print(cooldown)
+            if current_time - last_played < cooldown:
+                return
+
+        self.sounds[sound_name].play()
+        self.sound_cooldowns[sound_name] = current_time
 
     def start_music(self):
         pygame.mixer.music.play(-1)

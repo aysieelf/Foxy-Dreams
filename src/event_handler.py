c8e90f32e7b80a97e8aee9dec0ae1b73618e18de
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2025-01-07 13:13:38 +0200

    add: mouse position detection when clicking - add _handle_start_screen_click

--- src/event_handler.py
+++ src/event_handler.py
@@ -1,4 +1,5 @@
 from src.core.game_state import GameState
+from src.ui.renderer import Renderer
 from src.utils import constants as c
 from src.utils.screenshot import ScreenshotManager
 
@@ -19,9 +20,10 @@ class EventHandler:
 
     """
 
-    def __init__(self, game_state: GameState) -> None:
+    def __init__(self, game_state: GameState, renderer: Renderer) -> None:
         self.game_state = game_state
         self.screenshot_manager = ScreenshotManager()
+        self.renderer = renderer
 
     def handle_events(self) -> bool:
         """
@@ -33,6 +35,10 @@ class EventHandler:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return False
+            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:  # Left click
+                mouse_pos = pygame.mouse.get_pos()
+                if self.game_state.current_state == c.GameStates.START:
+                    self._handle_start_screen_click(mouse_pos)
             elif event.type == c.BONUS_SPAWN_EVENT:
                 self.game_state.bonus_star.spawn()
             elif event.type == c.BONUS_DE_SPAWN_EVENT:
@@ -40,6 +46,23 @@ class EventHandler:
             elif event.type == pygame.KEYDOWN:
                 return self._handle_keydown(event)
         return True
+
+    def _handle_start_screen_click(self, mouse_pos):
+        if self.renderer.start_button.is_clicked(mouse_pos):
+            self.game_state.set_state(c.GameStates.PLAYING)
+            self.game_state.sound_manager.play_sound("mouse-click")
+
+        elif self.renderer.multiplayer_button.is_clicked(mouse_pos):
+            self.game_state.toggle_multiplayer()
+            self.game_state.sound_manager.play_sound("mouse-click")
+
+        elif self.renderer.sound_button.is_clicked(mouse_pos):
+            self.game_state.sound_manager.toggle_sound()
+            self.game_state.sound_manager.play_sound("mouse-click")
+
+        elif self.renderer.music_button.is_clicked(mouse_pos):
+            self.game_state.sound_manager.toggle_music()
+            self.game_state.sound_manager.play_sound("mouse-click")
 
     def _handle_keydown(self, event: pygame.event.Event) -> bool:
         if event.key == pygame.K_ESCAPE:

3325be4763c1e2cfeb0d9cf208a527994bce14d6
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2025-01-10 16:36:36 +0200

    add: documentation to cloud.py

--- src/core/cloud.py
+++ src/core/cloud.py
@@ -1,5 +1,6 @@
 import random
 
+from src.core.fox import Fox
 from src.utils import constants as c
 
 import pygame.sprite
@@ -8,18 +9,12 @@ import pygame.sprite
 class Cloud(pygame.sprite.Sprite):
     """
     Class to represent a cloud in the game
-
-    Attributes:
-        image (pygame.Surface): The image of the cloud
-        rect (pygame.Rect): The rectangle of the cloud
     """
 
-    def __init__(self, player) -> None:
-        """
-        Initialize the cloud
-        """
+    def __init__(self, player: str, is_multiplayer: bool) -> None:
         super().__init__()
         self.player = player
+        self.is_multiplayer = is_multiplayer
         self.speed = c.BASE_SPEED * 0.35
         self.collision_cooldown = 0
         self.image: pygame.Surface = pygame.image.load(
@@ -37,7 +32,7 @@ class Cloud(pygame.sprite.Sprite):
 
         self.rect = self.image.get_rect()
 
-        self.shake_duration = 20
+        self.shake_duration = 30
         self.shake_intensity = 1
         self.is_shaking = False
         self.shake_start = 0
@@ -45,7 +40,13 @@ class Cloud(pygame.sprite.Sprite):
         self._set_position(player)
 
     @property
-    def hitbox(self):
+    def hitbox(self) -> pygame.Rect:
+        """
+        Get the hitbox of the cloud
+
+        Returns:
+            pygame.Rect: The hitbox of the cloud
+        """
         box = self.rect.copy()
         box.width = self.rect.width - c.CLOUD_HITBOX_WIDTH_DIFF
         box.height = self.rect.height + c.CLOUD_HITBOX_HEIGHT_DIFF
@@ -56,9 +57,9 @@ class Cloud(pygame.sprite.Sprite):
             box.right = self.rect.right
         return box
 
-    def update(self, fox) -> None:
+    def update(self, fox: Fox) -> None:
         """
-        Update the cloud
+        Update the cloud by moving it and shaking if necessary
         """
         self.update_shake()
         keys = pygame.key.get_pressed()
@@ -71,13 +72,13 @@ class Cloud(pygame.sprite.Sprite):
                 self.move("down")
 
         # Player 2 (arrows)
-        elif self.player == "player2":
+        elif self.player == "player2" and self.is_multiplayer:
             if keys[pygame.K_UP]:
                 self.move("up")
             if keys[pygame.K_DOWN]:
                 self.move("down")
 
-    def _set_position(self, player) -> None:
+    def _set_position(self, player: str) -> None:
         """
         Set the position of the cloud
         """
@@ -88,7 +89,13 @@ class Cloud(pygame.sprite.Sprite):
             self.rect.x = c.CLOUD_PLAYER2_X
             self.rect.y = c.CLOUD_Y
 
-    def move(self, direction):
+    def move(self, direction: str) -> None:
+        """
+        Move the cloud in the given direction
+
+        Args:
+            direction (str): The direction to move the cloud
+        """
         current_speed = self.speed
         if self.collision_cooldown > 0:
             current_speed *= 0.5
@@ -99,7 +106,16 @@ class Cloud(pygame.sprite.Sprite):
         elif direction == "down" and self.hitbox.bottom < c.HEIGHT + 4:
             self.rect.y += self.speed
 
-    def handle_fox_collision(self, fox):
+    def handle_fox_collision(self, fox: Fox) -> bool:
+        """
+        Handle the collision between the cloud and the fox
+
+        Args:
+            fox (Fox): The fox to check collision with
+
+        Returns:
+            bool: Whether a collision occurred
+        """
         if not fox.hitbox.colliderect(self.hitbox):
             return False
 
@@ -113,12 +129,24 @@ class Cloud(pygame.sprite.Sprite):
 
         return True
 
-    def _init_collision_state(self):
+    def _init_collision_state(self) -> None:
+        """
+        Initialize the state of the cloud when a collision occurs
+        """
         self.is_shaking = True
         self.shake_start = pygame.time.get_ticks()
         self.original_pos = self.rect.copy()
 
-    def _calculate_overlaps(self, fox):
+    def _calculate_overlaps(self, fox: Fox) -> tuple:
+        """
+        Calculate the overlaps between the cloud and the fox
+
+        Args:
+            fox (Fox): The fox to check overlaps with
+
+        Returns:
+            tuple: The overlaps in the x and y directions
+        """
         overlap_x = min(
             fox.hitbox.right - self.hitbox.left,
             self.hitbox.right - fox.hitbox.left,
@@ -129,18 +157,36 @@ class Cloud(pygame.sprite.Sprite):
         )
         return overlap_x, overlap_y
 
-    def _handle_side_collision(self, fox):
+    def _handle_side_collision(self, fox: Fox) -> None:
+        """
+        Handle the side collision between the cloud and the fox
+        """
         if self._is_valid_side_hit(fox):
             self._apply_side_bounce(fox)
         else:
             self._fix_invalid_side_hit(fox)
 
-    def _is_valid_side_hit(self, fox):
+    def _is_valid_side_hit(self, fox: Fox) -> bool:
+        """
+        Check if the side hit is valid
+
+        Args:
+            fox (Fox): The fox to check the side hit with
+
+        Returns:
+            bool: Whether the side hit is valid
+        """
         return (
             self.player == "player1" and fox.hitbox.centerx > self.hitbox.centerx
         ) or (self.player == "player2" and fox.hitbox.centerx < self.hitbox.centerx)
 
-    def _apply_side_bounce(self, fox):
+    def _apply_side_bounce(self, fox: Fox) -> None:
+        """
+        Apply the side bounce to the fox
+
+        Args:
+            fox (Fox): The fox to apply the side bounce to
+        """
         vertical_bounce = self._calculate_vertical_bounce(fox)
 
         if self.player == "player1":
@@ -152,7 +198,16 @@ class Cloud(pygame.sprite.Sprite):
         current_speed = fox.velocity.length()
         fox.velocity.scale_to_length(current_speed)
 
-    def _calculate_vertical_bounce(self, fox):
+    def _calculate_vertical_bounce(self, fox: Fox) -> float:
+        """
+        Calculate the vertical bounce for the fox
+
+        Args:
+            fox (Fox): The fox to calculate the vertical bounce for
+
+        Returns:
+            float: The vertical bounce for the fox
+        """
         relative_hit_point = (fox.hitbox.centery - self.hitbox.centery) / (
             self.hitbox.height / 2
         )
@@ -167,13 +222,25 @@ class Cloud(pygame.sprite.Sprite):
             )
         return vertical_bounce
 
-    def _fix_invalid_side_hit(self, fox):
+    def _fix_invalid_side_hit(self, fox: Fox) -> None:
+        """
+        Fix an invalid side hit by moving the fox to the correct position
+
+        Args:
+            fox (Fox): The fox to fix the invalid side hit for
+        """
         if self.player == "player1" and fox.hitbox.centerx > self.hitbox.centerx:
             fox.velocity.x = abs(fox.velocity.x)
         elif self.player == "player2" and fox.hitbox.centerx < self.hitbox.centerx:
             fox.velocity.x = -abs(fox.velocity.x)
 
-    def _handle_vertical_collision(self, fox):
+    def _handle_vertical_collision(self, fox: Fox) -> None:
+        """
+        Handle the vertical collision between the cloud and the fox
+
+        Args:
+            fox (Fox): The fox to handle the vertical collision with
+        """
         if fox.hitbox.centery < self.hitbox.centery:
             fox.rect.bottom = self.hitbox.top - c.FOX_HITBOX_DIFF * 0.75
         else:
@@ -181,17 +248,10 @@ class Cloud(pygame.sprite.Sprite):
         fox.velocity.y *= -1
         self.collision_cooldown = 3
 
-    def reset(self):
-        self._set_position(self.player)
-        self.collision_cooldown = 0
-        self.speed = c.BASE_SPEED * 0.35
-        self.shake_duration = 20
-        self.shake_intensity = 1
-        self.is_shaking = False
-        self.shake_start = 0
-        self.original_pos = None
-
-    def update_shake(self):
+    def update_shake(self) -> None:
+        """
+        Update the shake effect of the cloud
+        """
         if not self.is_shaking:
             return
 
@@ -208,3 +268,18 @@ class Cloud(pygame.sprite.Sprite):
 
         self.rect.centerx = self.original_pos.centerx + offset_x
         self.rect.centery = self.original_pos.centery + offset_y
+
+    def reset(self) -> None:
+        """
+        Reset the cloud to its initial state
+        """
+        self._set_position(self.player)
+        self.collision_cooldown = 0
+        self.speed = c.BASE_SPEED * 0.35
+        self.shake_duration = 20
+        self.shake_intensity = 1
+        self.is_shaking = False
+        self.shake_start = 0
+        self.original_pos = None
+
+
